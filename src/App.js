9cdf648317c13f6842caa411d336bdcf996dbfe9
Firdavs <firdavsdosiyorov3@gmail.com>
2023-05-22 19:53:35 +0500

    navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from "react";
 function App() {
   return (
     <div className="App">
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>home</li>
+          <li>shop</li>
+        </ul>
+      </nav>
       <h1>Salom Dunyo</h1>
     </div>
   );
